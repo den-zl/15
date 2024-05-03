31cdbb2c3372854dda35d416729b4ec4b99b9187
den-zl <lashindeniskb232@gmail.com>
2024-05-03 03:17:20 +0300

    bool predicate

--- libs/data_structures/matrix/matrix.h
+++ libs/data_structures/matrix/matrix.h
@@ -49,5 +49,11 @@ void selectionSortColsMatrixByColCriteria(matrix m, int (*criteria)(int*, int));
 
 bool isSquareMatrix(matrix *m);
 
+bool areTwoMatricesEqual(matrix *m1, matrix *m2);
+
+bool isEMatrix(matrix *m);
+
+bool isSymmetricMatrix(matrix *m);
+
 
 #endif
